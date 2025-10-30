352f76d4e926ada24aaebed2b4de1b8c60a44812
mbwika <collinsmbwika10@gmail.com>
2025-10-29 21:50:15 -0500

    style: standardize import quotes and formatting in app.module.ts and main.ts; update tsconfig.json include/exclude patterns

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { TopicsModule } from "./topics/topics.module";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TopicsModule } from './topics/topics.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Document } from "./entities/document.entity";
-import { Topic } from "./entities/topic.entity";
+import { Document } from './entities/document.entity';
+import { Topic } from './entities/topic.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: ":memory:",
+      type: 'sqlite',
+      database: ':memory:',
       entities: [Document, Topic],
       synchronize: true,
     }),
